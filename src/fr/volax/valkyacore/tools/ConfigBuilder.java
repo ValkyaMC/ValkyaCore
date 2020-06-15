fd48c88190c5349a2913365121472cf977f8cbbe
VolaxYT <volax6600mailpro@gmail.com>
2020-06-16 00:02:26 +0200

    ValkyaCore 1.0.03c

--- src/fr/volax/valkyacore/tools/ConfigBuilder.java
+++ src/fr/volax/valkyacore/tools/ConfigBuilder.java
@@ -7,73 +7,27 @@ public class ConfigBuilder {
     public static FileManager configs = new FileManager(ValkyaCore.getInstance());
 
     /**
-     * Return un String dans une config custom
+     * Return un Object dans une config custom
      *
-     * @param value  La direction du string à get
-     * @param config Config dans la quel il faut get la value
+     * @param path La direction du boolean à get
+     * @param config Object dans la quel il faut get la value
      * @return Contenue de "value" dans la config
      */
-    public static String getCString(String value, ConfigType config) {
-        return configs.getConfig(config.getConfigName()).get().getString(value).replaceAll("&", "§").replaceAll("%prefix%", ValkyaCore.PREFIX).replaceAll("%logger_prefix%", ValkyaCore.LOGGER_PREFIX);
-    }
-
-    /**
-     * Return un int dans une config custom
-     *
-     * @param value  La direction du int à get
-     * @param config Config dans la quel il faut get la value
-     * @return Contenue de "value" dans la config
-     */
-    public static int getCInt(String value, ConfigType config) {
-        return configs.getConfig(config.getConfigName()).get().getInt(value);
-    }
-
-    /**
-     * Return un Boolean dans une config custom
-     *
-     * @param value  La direction du boolean à get
-     * @param config Config dans la quel il faut get la value
-     * @return Contenue de "value" dans la config
-     */
-    public static Boolean getCBool(String value, ConfigType config) {
-        return configs.getConfig(config.getConfigName()).get().getBoolean(value);
+    public static Object getC(String path, ConfigType config) {
+        return configs.getConfig(config.getConfigName()).get(path);
     }
 
     /**
      * Modifier une donnée dans une config custom
      *
-     * @param value  La direction de la value à modifier
-     * @param data   String à modifier dans la config
+     * @param direction La direction de la value à modifier
+     * @param data Object à modifier dans la config
      * @param config Config dans la quel il faut modifier la value
      */
-    public static void setCString(String value, String data, ConfigType config) {
-        configs.getConfig(config.getConfigName()).set(value, data);
-        configs.getConfig(config.getConfigName()).save();
-    }
-
-    /**
-     * Modifier une donnée dans une config custom
-     *
-     * @param value  La direction de la value à modifier
-     * @param data   int à modifier dans la config
-     * @param config Config dans la quel il faut modifier la value
-     */
-    public static void setCInt(String value, int data, ConfigType config) {
-        configs.getConfig(config.getConfigName()).set(value, data);
+    public static void setC(String direction, Object data, ConfigType config) {
+        configs.getConfig(config.getConfigName()).set(direction, data);
         configs.getConfig(config.getConfigName()).save();
         configs.getConfig(config.getConfigName()).reload();
-    }
-
-    /**
-     * Modifier une donnée dans une config custom
-     *
-     * @param value  La direction de la value à modifier
-     * @param data   Boolean à modifier dans la config
-     * @param config Config dans la quel il faut modifier la value
-     */
-    public static void setCBool(String value, boolean data, ConfigType config) {
-        configs.getConfig(config.getConfigName()).set(value, data);
-        configs.getConfig(config.getConfigName()).save();
     }
 
     /**
